19fb44c14166086f0a503ed1d1e2f4c7338647f4
Nick B <imlearningai25@gmail.com>
2026-03-19 23:29:56 -0400

    v3 pushed

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask + Git + Jenkins + Docker + Kubernetes! v2"
+    return "Hello from Flask + Git + Jenkins + Docker + Kubernetes! v3"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
